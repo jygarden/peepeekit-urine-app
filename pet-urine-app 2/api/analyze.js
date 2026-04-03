7f8aa2cca1d28a8bd6905f766a96bba85076e801
jygarden <skaqod2@gmail.com>
2026-04-03 14:53:53 +0900

    Change export syntax from default to module.exports

--- pet-urine-app 2/api/analyze.js
+++ pet-urine-app 2/api/analyze.js
@@ -1,5 +1,5 @@
 // Vercel 서버리스 함수 - API 키를 서버에서 안전하게 보관
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS 허용
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
