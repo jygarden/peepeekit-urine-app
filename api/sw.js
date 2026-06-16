ee226b79fc98acfafb763d31a5f05a853b4da4bf
jygarden <skaqod2@gmail.com>
2026-06-16 16:42:25 +0900

    Update sw.js

--- api/sw.js
+++ api/sw.js
@@ -1,6 +1,6 @@
 // 건강어때 PWA Service Worker
 // 버전 올리면 모든 클라이언트가 자동 업데이트됨
-const SW_VERSION = 'v16-summary-and-normalize-2026-06-15';
+const SW_VERSION = 'v17-rich-summary-2026-06-15';
 
 // install: 즉시 활성화
 self.addEventListener('install', (event) => {
