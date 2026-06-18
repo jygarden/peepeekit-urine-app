16044ca6695d7bdf89dfdcdf20f5de4c42b3e718
jygarden <skaqod2@gmail.com>
2026-06-18 15:33:23 +0900

    Update sw.js

--- api/sw.js
+++ api/sw.js
@@ -1,6 +1,6 @@
 // 건강어때 PWA Service Worker
 // 버전 올리면 모든 클라이언트가 자동 업데이트됨
-const SW_VERSION = 'v34-rename-macro-to-basic-2026-06-15';
+const SW_VERSION = 'v36-settings-gear-icon-2026-06-15';
 
 // install: 즉시 활성화
 self.addEventListener('install', (event) => {
